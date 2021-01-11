752db404e148f7e30b85fef5d48bb4933d2a692c
Timmy-Bergkvist <59609304+Timmy-Bergkvist@users.noreply.github.com>
2021-01-11 21:29:48 +0100

    added functions to contract

--- contracts/Bank.sol
+++ contracts/Bank.sol
@@ -2,5 +2,45 @@ pragma solidity 0.7.5;
 
 contract Bank { 
     
+    mapping(address => uint) balance;
+    
+    function deposit() public payable returns (uint){
+        balance[msg.sender] += msg.value;
+   	    emit depositDone(msg.value, msg.sender); // emiting the event
+        return balance[msg.sender];
+    }
+    
+    function withdraw(uint amount) public returns (uint){
+        // get the balance from sender
+        require(balance[msg.sender] >= amount);
+        balance[msg.sender] -= amount;
+        msg.sender.transfer(amount);
+        return balance[msg.sender];
+    }
+    
+    function getAddress() public view returns (uint){
+        return balance[msg.sender];
+    }
+    
+    function transfer(address recipient, uint amount)public {
+        //check balance of msg.sender
+        require(balance[msg.sender] >= amount, "Balance not sufficient");
+        require(msg.sender != recipient, "Dont transfer money to yourself"); // sender cant send money to his/hers address
+        
+        uint previousSenderBalance = balance[msg.sender];
+        
+        _transfer(msg.sender, recipient, amount);
+        
+        governmentInstance.addTransaction{value: 1 ether}(msg.sender, recipient, amount);
+        
+        assert(balance[msg.sender] == previousSenderBalance - amount); //checks the code 
+    }
+    
+    function _transfer(address from , address to, uint amount) private {
+        
+        balance[from] -= amount; // reduce the balance
+        balance[to] += amount; // increase balance
+        
+    }
 
 }
